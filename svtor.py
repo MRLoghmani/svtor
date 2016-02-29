3e2d874ffc616e09b84d5e1f61dfcfe5e64fe442
Pranshu Gupta <pranshug@iitk.ac.in>
2016-02-29 21:06:40 +0530

    Now, we do not pass the feature cluster in constructTree recursive steps, instead we just pass the indices of the features that are in that cluster, saving memory by a factor of 128, since feature descriptors are lists of 128 numbers

--- svtor.py
+++ svtor.py
@@ -2,23 +2,32 @@
 # Group 13, CS676A, IITK
 #------------------------------------------------------------------------------------------------------------
 
-import cv2, time, os, math, operator, re
+import cv2, time, os, math, operator, re, sys
 import numpy as np
 from sklearn.cluster import KMeans
 
+
 #------------------------------------------------------------------------------------------------------------
-N = 20									# Number of samples to take as training set
+N = 100								# Number of samples to take as training set
 rootDir = 'data/full'					# The root directory of the dataset
-model =  KMeans(n_clusters=5)			# The KMeans Clustering Model
 nodes = {}								# List of nodes (list of SIFT descriptors)
 nodeIndex = 0							# Index of the last node for which subtree was constructed
 tree = {}								# A dictionary in the format - node: [child1, child2, ..]
 branches = 5							# The branching factor in the vocabulary tree
 leafClusterSize = 20					# Minimum size of the leaf cluster
 imagesInLeaves = {} 					# Dictionary in the format - leafID: [img1:freq, img2:freq, ..]
-doc = {}
-bestN = 4
-result = np.array([0,0,0,0])
+doc = {}								# 
+bestN = 4								#
+result = np.array([0,0,0,0])			#
+maxDepth = 5
+avgDepth = 0
+# If the values are supplied as command line arguments
+if len(sys.argv) == 3:
+	branches = int(sys.argv[1])
+	maxDepth = int(sys.argv[2])
+model =  KMeans(n_clusters=branches, n_jobs=4)	# The KMeans Clustering Model
+sift = cv2.xfeatures2d.SIFT_create(nfeatures=500) # SIFT Feature extractor model
+leafClusterSize = 2*branches
 #------------------------------------------------------------------------------------------------------------
 
 # Function to dump all the SIFT descriptors from training data in the feature space
@@ -30,10 +39,11 @@ def dumpFeatures(rootDir):
 			# print("Reading Image: " + dirName + "/" + fname)
 			img = cv2.imread(dirName + "/" + fname)
 			gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-			sift = cv2.xfeatures2d.SIFT_create()
+			
 			kp, des = sift.detectAndCompute(gray, None)
 			for d in des:
 				features.append(d)
+			# del kp, des
 			n = n + 1
 			if n >= N:
 				break
@@ -41,24 +51,33 @@ def dumpFeatures(rootDir):
 	return features
 
 # Function to construct the vocabulary tree
-def constructTree(node, cluster, depth):
+def constructTree(node, featuresIDs, depth):
 	global nodeIndex					# Changes made in this variable will be global 
 	global nodes						# Changes made in this variable will be global
 	global tree 						# Changes made in this variable will be global
 	global imagesInLeaves				# Changes made in this variable will be global
+	global avgDepth
 	tree[node] = []
-	if len(cluster) >= leafClusterSize:
+	if len(featuresIDs) >= leafClusterSize and depth < maxDepth :
+		# Here we will fetch the cluster from the indices and then use it to fit the kmeans
+		# And then just after that we will delete the cluster
+		# Using the array of indices instead of cluster themselves will reduce the memory usage by 128 times :)
+		cluster = []
+		for i in featuresIDs:
+			cluster.append(features[i])
 		model.fit(cluster)
-		childCluster = [[] for i in range(branches)]
-		for i in range(len(cluster)):
-			childCluster[model.labels_[i]].append(cluster[i])
+		del cluster
+		childFeatureIDs = [[] for i in range(branches)]
+		for i in range(len(featuresIDs)):
+			childFeatureIDs[model.labels_[i]].append(featuresIDs[i])
 		for i in range(branches):
 			nodeIndex = nodeIndex + 1
 			nodes[nodeIndex] = model.cluster_centers_[i]
 			tree[node].append(nodeIndex)
-			constructTree(nodeIndex, childCluster[i], depth + 1)
+			constructTree(nodeIndex, childFeatureIDs[i], depth + 1)
 	else:
 		imagesInLeaves[node] = {}
+		avgDepth = avgDepth + depth
 
 # Function to lookup a SIFT descriptor in the vocabulary tree, returns a leaf cluster
 def lookup(descriptor, node):
@@ -78,7 +97,7 @@ def tfidf(filename):
 	global imagesInLeaves
 	img = cv2.imread(dirName + "/" + fname)
 	gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sift = cv2.xfeatures2d.SIFT_create()
+	
 	kp, des = sift.detectAndCompute(gray, None)	
 	for d in des:
 		leafID = lookup(d, 0)
@@ -86,6 +105,7 @@ def tfidf(filename):
 			imagesInLeaves[leafID][filename] += 1
 		else:
 			imagesInLeaves[leafID][filename] = 1
+	# del kp, des
 
 # This function returns the weight of a leaf node
 def weight(leafID):
@@ -131,7 +151,7 @@ def match(filename):
 	q = {}
 	img = cv2.imread(dirName + "/" + fname)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sift = cv2.xfeatures2d.SIFT_create()
+	
 	kp, des = sift.detectAndCompute(gray, None)
 	for d in des:
 		leafID = lookup(d, 0)
@@ -153,15 +173,22 @@ def getImgID(s):
 
 #------------------------------------------------------------------------------------------------------------
 
-# print("Extracting Features: " + rootDir + " ...")
+print("Extracting Features: " + rootDir + " ...")
 features = dumpFeatures(rootDir)
 
-# print("Constructing Vocabulary Tree ... ")
+print("Constructing Vocabulary Tree ... ")
 root = features.mean(axis = 0)
 nodes[0] = root
-constructTree(0, features, 0)
+# Do not send the feature array itself but an array of indices into the construct tree function
+# This will save memory by a factor of 128, an awesome little trick, why didn't I think it before
+featuresIDs = [x for x in range(len(features))]
+constructTree(0, featuresIDs, 0)
 
-# print("Mapping images to leaf nodes of the tree ...")
+# del features
+
+avgDepth = int(avgDepth/len(imagesInLeaves))
+
+print("Mapping images to leaf nodes of the tree ...")
 for dirName, subdirList, fileList in os.walk(rootDir):
 	n = 0
 	for fname in fileList:
@@ -183,18 +210,18 @@ for img in doc:
 	for leafID in doc[img]:
 		doc[img][leafID] /= s
 
-# print("Finding Best Matches for each image ...")
+print("Finding Best Matches for each image ...")
 for dirName, subdirList, fileList in os.walk(rootDir):
 	n = 0
 	for fname in fileList:
 		filename = dirName + "/" + fname
 		group = match(filename)
 		# print(getImgID(filename), ": ", getImgID(group[0][0]), getImgID(group[1][0]), getImgID(group[2][0]), getImgID(group[3][0]))
-		# print(getImgID(filename), ": ", accuracy(getImgID(filename), getImgID(group[0][0]), getImgID(group[1][0]), getImgID(group[2][0]), getImgID(group[3][0])))
+		print(getImgID(filename), ": ", accuracy(getImgID(filename), getImgID(group[0][0]), getImgID(group[1][0]), getImgID(group[2][0]), getImgID(group[3][0])))
 		result = result + accuracy(getImgID(filename), getImgID(group[0][0]), getImgID(group[1][0]), getImgID(group[2][0]), getImgID(group[3][0]))
 		n = n + 1
 		if n >= N:
 			break
 
-print(N, branches, leafClusterSize = 20, result/1.0*N)
+print(branches, maxDepth, avgDepth, result/N)
 #------------------------------------------------------------------------------------------------------------
